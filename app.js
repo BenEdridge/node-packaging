c2785b198c76afc9877d53923042f13c7e3e895c
Ben Edridge <BenEdridge@users.noreply.github.com>
2020-08-21 20:05:02 +1000

    Add basic helper scripts and large lib imports

---
+++ app.js
@@ -0,0 +1,10 @@
+// Library bloat ;)
+
+const _ = require('lodash');
+const express = require('express');
+const axios = require('axios');
+const react = require('react');
+const reactdom = require('react-dom');
+const underscore = require('underscore');
+const mom = require('moment');
+const commander = require('commander');
